e8600ebcf3f95393cbc06b088ed829801f109216
Jonathan Phipps <jonathanmp25@gmail.com>
2017-08-18 18:19:28 -0500

    Add eslint and update code

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let express = require('express');
-let path = require('path');
-let logger = require('morgan');
-let exphbs = require('express-handlebars');
-let bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
-let app = express();
+const hbsHelpers = require('./lib/hbs-helpers');
+
+const app = express();
 
 // handlebars view engine setup
-app.engine('hbs', exphbs({extname: 'hbs', helpers: require('./lib/hbs-helpers')}));
+app.engine('hbs', exphbs({ extname: 'hbs', helpers: hbsHelpers }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -18,24 +20,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
 
-  //Handle ENOENT errors
-  if(err.code === 'ENOENT' && err.path.indexOf('default.json') != -1){
-    err.details = 'If no path is specified, this program uses `default.json` for data.  Either rename your data file to default.json or specify the filename in the query params';
+  // Handle ENOENT errors
+  if (err.code === 'ENOENT' && err.path.indexOf('default.json') !== -1) {
+    err.details = 'If no path is specified, this program uses `default.json` for data.  Either rename your data file to default.json or specify the filename in the query params'; // eslint-disable-line no-param-reassign
   }
   res.render('error', err);
 });
